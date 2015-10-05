5d617ffa2053061394fa580579568befd5da61f8
quach-duc-mulodo <quach.duc@mulodo.com>
2015-10-05 14:23:16 +0700

    Generate scaffold tweets

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -7,7 +7,7 @@ $baseDir = dirname($vendorDir);
 
 return array(
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
-    'CreateTweetsTable' => $baseDir . '/database/migrations/2015_09_28_035730_create_tweets_table.php',
+    'CreateTweetsTable' => $baseDir . '/database/migrations/2015_10_05_071732_create_tweets_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
     'DatabaseSeeder' => $baseDir . '/database/seeds/DatabaseSeeder.php',
     'File_Iterator' => $vendorDir . '/phpunit/php-file-iterator/src/Iterator.php',
