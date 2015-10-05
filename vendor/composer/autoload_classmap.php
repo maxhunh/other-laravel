f0d5b1d5b63da72cfe8b5d46d2f4804667358f30
quach-duc-mulodo <quach.duc@mulodo.com>
2015-10-05 14:25:55 +0700

    Make table for queue in database

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'CreateJobsTable' => $baseDir . '/database/migrations/2015_10_05_072510_create_jobs_table.php',
     'CreatePasswordResetsTable' => $baseDir . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
     'CreateTweetsTable' => $baseDir . '/database/migrations/2015_10_05_071732_create_tweets_table.php',
     'CreateUsersTable' => $baseDir . '/database/migrations/2014_10_12_000000_create_users_table.php',
